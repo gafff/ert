9b507ace1ff6282747634a87377e8eb10359ade1
gafff <39094640+gafff@users.noreply.github.com>
2018-05-14 19:19:31 +0500

    Create ComponentForm.java

---
+++ src/main/java/kr/KR/ComponentForm.java
@@ -0,0 +1,32 @@
+/**
+ * объявление пакета
+ */
+package kr.KR;
+/**
+ * Библиотеки необходимые для работы
+ */
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+/**
+ * Объявление публичного класса для создания окна 
+ */
+public class ComponentForm extends JFrame {
+	/**
+	 * Описание компонентов формы
+	 */
+	JPanel contentPane;
+	JPanel Panel1;
+	JPanel Panel2;
+	static JLabel label_1;
+	static JLabel label_2;
+	static JLabel label_3;
+	static JTextField TxtBox1;
