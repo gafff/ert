4b0ed81b8ec88a7f96970a74bcfbe5734267c1e4
gafff <39094640+gafff@users.noreply.github.com>
2018-05-14 19:20:54 +0500

    Create MathForm.java

---
+++ src/main/java/kr/KR/MathForm.java
@@ -0,0 +1,16 @@
+/**
+ * объявление пакета
+ */
+package kr.KR;
+/**
+ * Библиотеки необходимые для работы.
+ */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/**
+ * Объявление публичного класса для создания зависимости между классами и выполнение вычислений.
+ */
+public class MathForm implements ActionListener {
